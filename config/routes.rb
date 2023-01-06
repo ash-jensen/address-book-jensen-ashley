1d5efcd1825f095446ffe01f0d9ae447c9e38d4b
ashleyjensen <ashleyjensen@hey.com>
2023-01-06 14:20:46 -0600

    Add addressbook controller

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
+  # get 'addressbook/index'
+  root 'addressbook#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
