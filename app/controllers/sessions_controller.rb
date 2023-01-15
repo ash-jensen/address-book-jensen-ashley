cb6670c43465dac10c92ade3dac6f51e66109cb2
ashleyjensen <ashleyjensen@hey.com>
2023-01-15 10:12:04 -0700

    Fix sign up/sign on with users/essions

---
+++ app/controllers/sessions_controller.rb
@@ -0,0 +1,18 @@
+class SessionsController < ApplicationController
+  def create
+    user = User.find_by(email: params[:session][:email].downcase)
+    if user && user.authenticate(params[:session][:password])
+      session[:user_id] = user.id
+      flash[:notice] = "Sign in Successful."
+      redirect_to root_path
+    else
+      flash.now[:alert] = "Incorrect email or password, please try again."
+      render 'new'
+    end
+  end
+  def destroy
+    session[:user_id] = nil
+    flash[:notice] = "Sign out successful."
+    redirect_to sign_in_path
+  end
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,5 @@
 class User < ApplicationRecord
+  validates :email, :password, presence: true
   has_secure_password
+  has_many :people
 end
