72fb4685c398ea39fa7e07e292fe64d1069993d2
ashleyjensen <ashleyjensen@hey.com>
2023-01-16 21:36:02 -0600

    Fix routes and cleanup

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,18 @@
 Rails.application.routes.draw do
+  # Users Routes
   resources :users, except: [:new]
-  get "sign_up", to: "users#new"
+  get "sign-up", to: "users#new"
+  
+  # Sessions Routes
+  get "sign-in", to: "sessions#new"
+  post "sign-in", to: "sessions#create"
+  delete "sign-out", to: "sessions#destroy"
+  
+  # Addresses Routes
+  resources :addresses
 
-
-
+  # People Routes
   resources :people
-  # get 'addressbook/index'
-  root 'addressbook#index'
+  root 'people#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
